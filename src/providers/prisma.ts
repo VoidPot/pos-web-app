154984549509b9cec9419cbfe370c1848c25991c
Zakir Hussain <itsjakirhussain@gmail.com>
2024-01-12 02:56:00 +0530

    prisma error fix

--- src/providers/prisma.ts
+++ src/providers/prisma.ts
@@ -25,45 +25,6 @@ prisma.$on("query", (e) => {
   console.log("Duration: " + e.duration + "ms");
 });
 
+globalThis.prisma = prisma;
+
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
-async function connect() {
-  console.log("DATABASE :: Initializing prisma connecting");
-  await prisma
-    .$connect()
-    .then(() => {
-      console.log(
-        "DATABASE :: Prisma connected to the database server ✅ ✅ ✅"
-      );
-    })
-    .catch(async (error: any) => {
-      await prisma.$disconnect();
-      console.error(
-        "DATABASE :: prisma failed to connect to the database",
-        error
-      );
-      // process.exit(1);
-    });
-
-  process.on("SIGINT", async function () {
-    await prisma.$disconnect().then(() => {
-      console.error(
-        "DATABASE :: Prisma disconnecting from the database server due to SIGINT"
-      );
-      process.exit(0);
-    });
-  });
-}
-
-async function disconnect() {
-  return await prisma.$disconnect().then(() => {
-    console.error("DATABASE :: Prisma disconnecting from the database server");
-  });
-}
-
-export const database = {
-  connect,
-  disconnect,
-};
